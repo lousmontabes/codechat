62117a324cf5d5414df0adf7018db620a52209ac
Lluis Montabes <lluis.456@gmail.com>
2016-10-04 16:31:57 +0200

    Removed logo from header

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
-<div id="header"><img src="images/logo.svg" id="logo"><a href="home.php">codechat</a> <span id="userinfo"><b><a style="border:none; opacity:1;" href="profile.php?u=<?php echo $activeuser_id?>"><?php echo $activeuser_name ?></a></b> (<a href="logout.php">Log out</a>)
+<div id="header"><a href="home.php">codechat</a> <span id="userinfo"><b><a style="border:none; opacity:1;" href="profile.php?u=<?php echo $activeuser_id?>"><?php echo $activeuser_name ?></a></b> (<a href="logout.php">Log out</a>)
 
 <a href="profile.php?u=<?php echo $activeuser_id?>">
 <div class="smallavatar" style="margin:0; margin-top:-20px;">
