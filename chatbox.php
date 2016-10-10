23e300a5564a63003abc6ea608b12248ec92547b
Lluis Montabes <lluis.456@gmail.com>
2016-10-10 18:31:05 +0200

    Styled token message.

--- chatbox.php
+++ chatbox.php
@@ -76,7 +76,7 @@ else echo "<img src='images/placeholder". $activeuser_id % 5 .".gif'>";
 <div id="area">
 
     <div id="tokenmessage">
-        This chatroom's token is <b><?php echo $chat_token?></b>.
+        This chatroom's token is <b><?php echo $chat_token?></b>. Share it with whoever you want to invite them in.
     </div>
 
 <?php
