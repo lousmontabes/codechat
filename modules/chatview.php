db387c506294f51f3ae2267325dd4f5df424d247
Lluis Montabes <lluis.456@gmail.com>
2016-11-01 17:04:57 +0100

    WIP

--- modules/chatview.php
+++ modules/chatview.php
@@ -1,4 +1,4 @@
-<div class="card" id="card<?php echo $i ?>" onclick="cardClicked(<?php echo $i ?>, <?php echo $chat['token'] ?>)">
+<div class="card" id="card<?php echo $i ?>" onclick="cardClicked(<?php echo $i ?>, "<?php echo $chat['token'] ?>")">
     <div class="row">
         <span class="chatTitle"><?php echo $chat['name'] ?></span>
         <div class="tag"><?php echo $chat['language'] ?></div>
