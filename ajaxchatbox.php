cd93b7e9d2f9579ca7df87a1cdff13ecc6e1bf96
Lluis Montabes <lluis.456@gmail.com>
2016-11-01 23:59:49 +0100

    WIP

--- ajaxchatbox.php
+++ ajaxchatbox.php
@@ -148,7 +148,7 @@ else echo "<img src='images/placeholder". $author_id % 5 .".gif'>";
         document.title = 'Codechat / <?php echo html_entity_decode($chat_name)?>';
         setTimeout(Prism.highlightAll,1);
         setTimeout('$("#area").css("opacity",1)',20);
-        setInterval(getMessageCount, 100);
+        setTimeout('setInterval(getMessageCount, 100)', 200);
     });
 
     $(window).scroll(function(event){
