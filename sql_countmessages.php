040c336ae9d101fb74a7322c312d468b67eb74fc
Lluis Montabes <lluis.456@gmail.com>
2016-11-01 22:59:33 +0100

    WIP

--- sql_countmessages.php
+++ sql_countmessages.php
@@ -11,7 +11,7 @@ $con = new mysqli($server, $username, $password, $db);
 
 $chat_id = $_GET['chat_id'];
 
-$result = mysqli_query($con, "SELECT if FROM messages WHERE chat = $chat_id");
+$result = mysqli_query($con, "SELECT id FROM messages WHERE chat = $chat_id");
 $messagecount = mysqli_num_rows($result);
 
 echo $messagecount;
