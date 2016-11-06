cb1b26fc4d1d3424bb8a7f8daf756dbefb11ba8b
Lluis Montabes <lluis.456@gmail.com>
2016-11-06 02:55:26 +0100

    Testing dynamic token search bar

--- sql-getchatbytoken.php
+++ sql-getchatbytoken.php
@@ -6,6 +6,15 @@
  * Time: 2:45
  */
 
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$con = new mysqli($server, $username, $password, $db);
+
 $result = mysqli_query($con, "SELECT * FROM chats WHERE token = '{$_GET['token']}'");
 while($row = mysqli_fetch_array($result)){
     print_r($row);
