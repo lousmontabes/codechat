2808fa7167d7c8dfed06e1bc2a6febeecde2da69
Lluis Montabes <lluis.456@gmail.com>
2016-11-11 13:58:19 +0100

    Fixed IP tracking and added extra fields for the visits table

--- updatestats.php
+++ updatestats.php
@@ -15,8 +15,34 @@ $db = substr($url["path"], 1);
 
 $con = new mysqli($server, $username, $password, $db);
 
-$user_ip = $_SERVER['REMOTE_ADDR'];
+// Function to get the client IP address
+function get_client_ip() {
+    $ipaddress = '';
+    if (isset($_SERVER['HTTP_CLIENT_IP']))
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    else if(isset($_SERVER['REMOTE_ADDR']))
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
 
-mysqli_query($con,"INSERT INTO visits(page, ip) VALUES ('register.php', '{$user_ip}')");
+$user_ip = get_client_ip();
+$details = json_decode(file_get_contents("http://ipinfo.io/{$user_ip}/json"));
+
+$country = $details->country;
+$region = $details->region;
+$city = $details->city;
+$location = $details->loc;
+
+mysqli_query($con,"INSERT INTO visits(page, ip, country, region, city, location) VALUES ('register.php', '{$user_ip}', '{$country}', '{$region}', '{$city}', '{$location}')");
 
 ?>
