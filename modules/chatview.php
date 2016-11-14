f67ca0c5550bb63fa871a50e5d2e02174e243ea1
Lluis Montabes <lluis.456@gmail.com>
2016-11-14 13:36:58 +0100

    WIP

--- modules/chatview.php
+++ modules/chatview.php
@@ -1,12 +1,21 @@
 <div class="card" id="card<?php echo $i ?>" onclick="cardClicked(<?php echo $i ?>, '<?php echo $chat['token'] ?>')">
-    <div class="row">
-        <span class="chatTitle"><?php echo $chat['name'] ?></span>
-        <div class="tag"><?php echo $chat['language'] ?></div>
+    <div class="leftside">
+        <div class="row">
+            <span class="chatTitle"><?php echo $chat['name'] ?></span>
+            <div class="tag"><?php echo $chat['language'] ?></div>
+        </div>
+        <div class="stats">
+            <!--<li class="highlighted">[AMOUNT] new messages</li>-->
+            <li><?php echo $chat['messages'] ?> message<?php if($chat['messages'] > 1){?>s<? } ?></li>
+            <!--<li>[AMOUNT] users</li>-->
+            <!--<li>[AMOUNT] active right now</li>-->
+        </div>
     </div>
-    <div class="stats">
-        <!--<li class="highlighted">[AMOUNT] new messages</li>-->
-        <li><?php echo $chat['messages'] ?> message<?php if($chat['messages'] > 1){?>s<? } ?></li>
-        <!--<li>[AMOUNT] users</li>-->
-        <!--<li>[AMOUNT] active right now</li>-->
+    <div class="rightside">
+        <?php if ($isSaved){ ?>
+            remove
+        <? }else{ ?>
+            save
+        <?php } ?>
     </div>
 </div>
