4217d6b69a0ef43593f76a6d20975b8749350652
Lluis Montabes <lluis.456@gmail.com>
2017-02-06 13:40:15 +0100

    wip

--- live.php
+++ live.php
@@ -136,15 +136,12 @@ $chat_language = $row['language'];
             padding:0 1.5em;
             left:0;
             bottom:0;
-            width:100%;
+            width:25vw;
             height:6em; /* 10vh (shade) */
             overflow:hidden;
             font-family:open sans;
             font-size:14px;
             color:white;
-            background: dimgrey; /* fallback for old browsers */
-            background: -webkit-linear-gradient(to top, rgba(0,0,0,0.75), rgba(0,0,0,0)); /* Chrome 10-25, Safari 5.1-6 */
-            background: linear-gradient(to top, rgba(0,0,0,0.75), rgba(0,0,0,0)); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
         }
 
         #messages{
