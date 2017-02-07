7bb8de54b208ced7e9f5ef53e15ff334f39f05e1
Lluis Montabes <lluis.456@gmail.com>
2017-02-07 20:54:56 +0100

    wip

--- main.php
+++ main.php
@@ -218,7 +218,7 @@
             padding:1.5rem;
             position:absolute;
             bottom:0;
-            width:21.3vw;
+            width:20.3vw;
         }
 
         #newChatCard:hover, #newChatCard.highlighted{
