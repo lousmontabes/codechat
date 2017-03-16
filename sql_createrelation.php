22f565b520aa3ae1edc70a0c8d6d178c26e7ad89
Lluis Montabes <lluis.456@gmail.com>
2017-03-16 11:04:50 +0100

    Added check when saving a chatroom to make sure it is not already saved by the user

--- sql_createrelation.php
+++ sql_createrelation.php
@@ -18,13 +18,32 @@ $db = substr($url["path"], 1);
 $con = new mysqli($server, $username, $password, $db);
 
 $chat_id = $_GET['chat_id'];
-$result = mysqli_query($con, "INSERT INTO `relations`(`user`, `chat`) VALUES ($activeuser_id, $chat_id)");
+$alreadyExists = false;
 
-if ($result){
-    echo true;
+$result = mysqli_query($con, "SELECT * FROM relations WHERE user = $activeuser_id");
+
+if (mysqli_num_rows($result) == 0){
+    $alreadyExists = false;
+}else{
+    while ($relation = mysqli_fetch_array($result) && !$alreadyExists) {
+
+        $relation_chat = $relation['chat'];
+        $found = ($chat_id = $relation_chat);
+
+    }
 }
-else{
-    echo false;
+
+if(!$alreadyExists){
+
+    $result = mysqli_query($con, "INSERT INTO `relations`(`user`, `chat`) VALUES ($activeuser_id, $chat_id)");
+
+    if ($result){
+        echo true;
+    }
+    else{
+        echo false;
+    }
+
 }
 
 ?>
