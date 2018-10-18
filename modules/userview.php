a5293bc802b0f5ecd9eb1a3463252dbf9b6c326b
Lluís Montabes <lluis.456@gmail.com>
2018-10-18 11:48:32 +0200

    Made link to placeholder1.gif relative

--- modules/userview.php
+++ modules/userview.php
@@ -1,4 +1,4 @@
 <div class="centered" id="userbar">
-    <img src="http://www.codechat.co/images/placeholder1.gif" style="float:left; width:40px; border-radius:20px; margin-right:20px">
+    <img src="images/placeholder1.gif" style="float:left; width:40px; border-radius:20px; margin-right:20px">
     <?php echo $activeUser['name'] ?><br><a href="/logout.php">Log out</a>
 </div>
